e161ea1357f9294d68ad3c7c00c9380320bcd670
Austin Jenchi <ajench9@gmail.com>
2017-09-07 19:22:30 -0700

    Add a basic main class

---
+++ src/main/java/io/github/timtim17/makeamountlimitedcoins/MakeAmountMain.java
@@ -0,0 +1,10 @@
+package io.github.timtim17.makeamountlimitedcoins;
+
+/**
+ * @author Austin Jenchi (timtim17)
+ */
+public class MakeAmountMain {
+    public static void main(String[] args) {
+        // TODO: Method stub
+    }
+}
